671be9d47d2b91a04093f560f8dfc5b597889402
eliasbhuiyan <eliasbhuiyan21@gmail.com>
2025-09-24 01:54:54 +0600

    UI touch

--- agent-client/app/(dashboard)/modify-image/page.js
+++ agent-client/app/(dashboard)/modify-image/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ModifyImage = () => {
   return (
     <div>
-      ModifyImage: Resize, Upscale, LowScale, Wattermark as logo or text with opacity, drag and drop watermark positioning
+      Modify Image: Resize, Upscale, LowScale, Wattermark as logo or text with opacity, drag and drop watermark positioning
     </div>
   )
 }
