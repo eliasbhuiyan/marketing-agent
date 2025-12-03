cfaa17418fb319fead0719ff4fea826d691c5e37
unknown <eliasbhuiyan21@gmail.com>
2025-12-03 14:00:15 +0600

    callback

--- agent-client/lib/hooks/useSingleHistory.js
+++ agent-client/lib/hooks/useSingleHistory.js
@@ -4,32 +4,30 @@ const { useState, useEffect, useCallback } = require("react");
 const { default: apiClient } = require("../api");
 
 const useSingleHistory = () => {
-    const searchParams = useSearchParams();
-    const historyId = searchParams.get("single-h-id");
-    const [historyData, setHistoryData] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(null);
+  const searchParams = useSearchParams();
+  const historyId = searchParams.get("single-h-id");
+  const [historyData, setHistoryData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  //   use useCallback to memoize the function to avoid unnecessary re-renders
+  const fetchHistoryData = useCallback(async () => {
+    setLoading(true);
     if (!historyId) return { historyData, loading, error };
-    //   use useCallback to memoize the function to avoid unnecessary re-renders
-    const fetchHistoryData = useCallback(async () => {
-        setLoading(true);
-        try {
-            const response = await apiClient.usageHistory.getSingleHistory(
-                historyId
-            );
-            setHistoryData(response);
-        } catch (err) {
-            setError(err);
-        } finally {
-            setLoading(false);
-        }
-    }, [historyId]);
-    //   use useEffect to fetch the history data when the component mounts
-    useEffect(() => {
-        fetchHistoryData();
-    }, [fetchHistoryData]);
+    try {
+      const response = await apiClient.usageHistory.getSingleHistory(historyId);
+      setHistoryData(response);
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, [historyId]);
+  //   use useEffect to fetch the history data when the component mounts
+  useEffect(() => {
+    fetchHistoryData();
+  }, [fetchHistoryData]);
 
-    return { historyData, loading, error };
+  return { historyData, loading, error };
 };
 
 export default useSingleHistory;
